ac5c430b91fc2fdb9135e85fa355d180f4715923
shamim07-ansari <124763751+shamim07-ansari@users.noreply.github.com>
2023-07-14 21:39:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let i = 0;
+let j = 0;
+
+while(i < str.length) {
+  if(str[i] === str[j]) {
+    i++;
+  }
+  else {
+    if(i-j <= 1){
+      console.log(str[j]);
+      break;
+    }
+    j=i;
+    i++;
+  }
+}
